fba055ff289b288941c787a1e9f012a172738f00
Sergey Panchenko <forelock10@gmail.com>
2022-03-29 14:46:38 +0300

    NewTests

---
+++ ClassHelper/CalcDebt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library2ISP11_17_ZeyArt_DanArt.ClassHelper
+{
+    public static class CalcDebt
+    {
+        public static double Debt(DateTime dateStart, double price)
+        {
+            int dayCount = (DateTime.Now - dateStart).Days;
+            double debt = (dayCount - 30) * 0.01 * price;
+            if (debt > 0)
+            {
+                return debt;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
